69fcf98598317352b9810274eb51f6841b98689a
franxx32 <43724124+franxx32@users.noreply.github.com>
2023-04-15 11:34:57 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
 import { serve } from "https://deno.land/std@0.176.0/http/server.ts";
 
 let port = parseInt(Deno.env.get("PORT") ?? "8000");
-const s = serve({ port });
-
 console.log(`http://localhost:${port}/`);
 
-for await (const req of s) {
-	req.respond({ body: "Choo Choo! Welcome to your Deno app\n" });
+function logError(msg: string) {
+  console.log(msg);
+  Deno.exit(1);
 }
+function handleConnected() {
+  console.log("Connected to client ...");
+}
+function handleMessage(ws: WebSocket, data: string) {
+  console.log("CLIENT >> " + data);
+  const reply = prompt("Server >> ") || "No reply";
+  if (reply === "exit") {
+    return ws.close();
+  }
+  ws.send(reply as string);
+}
+function handleError(e: Event | ErrorEvent) {
+  console.log(e instanceof ErrorEvent ? e.message : e.type);
+}
+async function reqHandler(req: Request) {
+  console.log(1);
+  const { socket: ws, response } = Deno.upgradeWebSocket(req);
+  ws.onopen = () => handleConnected();
+  ws.onmessage = (m) => handleMessage(ws, m.data);
+  ws.onclose = () => logError("Disconnected from client ...");
+  ws.onerror = (e) => handleError(e);
+  return response;
+}
+console.log("Waiting for client ...");
+serve(reqHandler, { port });
